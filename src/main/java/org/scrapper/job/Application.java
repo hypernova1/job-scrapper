add8f5dfcca4fd26231e1568a092afee52eae5cb
hypernova1 <chtlstjd01@gmail.com>
2020-11-26 20:16:03 +0900

    init

---
+++ src/main/java/org/scrapper/job/Application.java
@@ -0,0 +1,21 @@
+package org.scrapper.job;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("Hello World");
+    }
+}
