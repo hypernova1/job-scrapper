de72be60612526c051ab5d54c1ede2cdef4d5e8a
hypernova1 <chtlstjd01@gmail.com>
2020-11-27 21:51:33 +0900

    feat: 로켓펀치 url 생성 메서드 작성

--- src/main/java/org/scrapper/job/context/JobScrapper.java
+++ src/main/java/org/scrapper/job/context/JobScrapper.java
@@ -2,10 +2,16 @@ package org.scrapper.job.context;
 
 import org.scrapper.job.service.RocketPunchScrapper;
 
+import java.io.IOException;
+
 public class JobScrapper {
 
     public static void execute() {
-        RocketPunchScrapper.execute();
+        try {
+            RocketPunchScrapper.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/org/scrapper/job/service/RocketPunchScrapper.java
+++ src/main/java/org/scrapper/job/service/RocketPunchScrapper.java
@@ -7,14 +7,38 @@ import java.io.IOException;
 
 public class RocketPunchScrapper {
 
-    public static void execute() {
-        try {
-            String url = "https://www.rocketpunch.com/api/jobs/template?location=서울특별시&specialty=Java&specialty=JSP&tag=웹서비스&tag=e-commerce";
-            Document document = Jsoup.connect(url).ignoreContentType(true).get();
-            System.out.println(document.getAllElements());
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void execute() throws IOException {
+
+        boolean isTodayWrite = true;
+        String location = "서울특별시";
+        int page = 1;
+        String[] languages = { "Java", "Spring" };
+        String[] tags = { "웹서비스", "e-commerce" };
+        String url = createUrl(location, page, languages, tags);
+
+        Document document = Jsoup.connect(url).ignoreContentType(true).get();
+
+        System.out.println(document.getAllElements());
+//        while (isTodayWrite) {
+//            if (true) {
+//                isTodayWrite = false;
+//            }
+//        }
+
+    }
+
+    public static String createUrl(String location, int page, String[] languages, String[] tags) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("https://www.rocketpunch.com/api/jobs/template?");
+        sb.append("&location=").append(location);
+        sb.append("&page=").append(page);
+        for (String language : languages) {
+            sb.append("&specialty=").append(language);
         }
+        for (String tag : tags) {
+            sb.append("&tag=").append(tag);
+        }
+        return sb.toString();
     }
 
 }
